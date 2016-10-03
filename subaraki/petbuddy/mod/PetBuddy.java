5ee0dbcfbb20a7eb4d7f7711ae71c98941462072
ArtixAllMighty <robbeenaxel@hotmail.com>
2016-10-03 11:05:55 +0200

    changed version nr

--- subaraki/petbuddy/mod/PetBuddy.java
+++ subaraki/petbuddy/mod/PetBuddy.java
@@ -10,7 +10,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 import subaraki.petbuddy.capability.PetInventoryCapability;
 import subaraki.petbuddy.gui.GuiHandler;
 import subaraki.petbuddy.gui.handler.KeyHandler;
-import subaraki.petbuddy.hooks.PetDeathTracker;
+import subaraki.petbuddy.hooks.CoolDownTracker;
 import subaraki.petbuddy.hooks.PlayerTracker;
 import subaraki.petbuddy.hooks.StowOrSummonLogic;
 import subaraki.petbuddy.network.NetworkHandler;
@@ -21,7 +21,7 @@ public class PetBuddy {
 
 	public static final String MODID = "petbuddy";
 	public static final String NAME = "Pet Buddy";
-	public static final String VERSION = "1.10.2 v1";
+	public static final String VERSION = "1.10.2 v2";
 	public static final String DEPENDENCY = "required-after:subcommonlib";
 
 	private static final String client = "subaraki.petbuddy.proxy.ClientProxy";
@@ -46,7 +46,7 @@ public class PetBuddy {
 		new StowOrSummonLogic();
 		new PlayerTracker();
 		new NetworkHandler();
-		new PetDeathTracker();
+		new CoolDownTracker();
 		
 		new KeyHandler();
 	}
