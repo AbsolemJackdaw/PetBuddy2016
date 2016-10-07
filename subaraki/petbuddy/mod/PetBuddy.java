66ea5fc2e930e15c03e189ab4fbed3b2e14fefc1
ArtixAllMighty <robbeenaxel@hotmail.com>
2016-10-07 18:29:16 +0200

    adjusted version

--- subaraki/petbuddy/mod/PetBuddy.java
+++ subaraki/petbuddy/mod/PetBuddy.java
@@ -24,7 +24,7 @@ public class PetBuddy {
 
 	public static final String MODID = "petbuddy";
 	public static final String NAME = "Pet Buddy";
-	public static final String VERSION = "1.10.2 v2.1";
+	public static final String VERSION = "1.10.2 v2.2";
 	public static final String DEPENDENCY = "required-after:subcommonlib";
 
 	private static final String client = "subaraki.petbuddy.proxy.ClientProxy";
